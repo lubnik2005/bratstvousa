0c98a5eca48dd024e08bc735b8078bed9df59adb
lubnik2005 <25041454+lubnik2005@users.noreply.github.com>
2024-12-17 16:32:56 -0800

    Moved database url to grab from process.env

--- app/src/lib/server/db/index.ts
+++ app/src/lib/server/db/index.ts
@@ -1,7 +1,19 @@
+// import { drizzle } from 'drizzle-orm/postgres-js';
+// import postgres from 'postgres';
+// import { env } from '$env/dynamic/private';
+// console.log('env DATABASE_URL:', env.DATABASE_URL);
+// if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+// const client = postgres(env.DATABASE_URL);
+// export const db = drizzle(client);
+//
+
+// WARNING: the above is the original, svelte uses env, instead of process.env
+
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import { env } from '$env/dynamic/private';
-console.log('env DATABASE_URL:', env.DATABASE_URL);
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+console.log('env DATABASE_URL:', process.env.DATABASE_URL);
+if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 const client = postgres(env.DATABASE_URL);
 export const db = drizzle(client);
+
