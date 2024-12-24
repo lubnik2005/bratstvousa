2d169e11a200b3f004d20fc4983ded807d7a16a8
lubnik2005 <25041454+lubnik2005@users.noreply.github.com>
2024-12-24 13:32:42 -0800

    Removed db console log

--- app/drizzle.config.ts
+++ app/drizzle.config.ts
@@ -3,7 +3,6 @@ import { secret } from '@aws-amplify/backend';
 
 const database_url =
 	process.env.DATABASE_URL ?? (secret('DATABASE_URL') as unknown as string | undefined);
-console.log('DATABASE_URL:', process.env.DATABASE_URL ?? secret('DATABASE_URL'));
 if (!database_url) throw new Error('DATABASE_URL is not set');
 
 export default defineConfig({
