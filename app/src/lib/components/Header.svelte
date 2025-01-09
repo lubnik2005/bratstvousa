0f849f1891f58a1a6a0a0d57b4b1ff072ca555e4
lubnik2005 <25041454+lubnik2005@users.noreply.github.com>
2025-01-09 11:11:06 -0800

    Made subtitle for Header optional

--- app/src/lib/components/Header.svelte
+++ app/src/lib/components/Header.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
 	export let title = 'Отдел';
-	export let subtitle;
+	export let subtitle = undefined;
 </script>
 
 <!-- Page Header Start -->
 <div class="container-fluid page-header">
 	<div class="container">
 		<h1 class="display-3 mb-3">{title}</h1>
+    {#if subtitle}
 		<p class="col-lg-8">{subtitle}</p>
+    {/if}
 		<!-- <nav aria-label="breadcrumb"> -->
 		<!-- 	<ol class="breadcrumb mb-0"> -->
 		<!-- 		<li class="breadcrumb-item"><a class="text-body" href="#">Home</a></li> -->
