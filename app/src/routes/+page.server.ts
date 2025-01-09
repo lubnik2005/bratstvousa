fc798671ded03e995b89007a8d16465aa2215080
lubnik2005 <25041454+lubnik2005@users.noreply.github.com>
2025-01-08 21:59:04 -0800

    Fixed home page list

--- app/src/routes/+page.server.ts
+++ app/src/routes/+page.server.ts
@@ -1,45 +1,33 @@
 import { db } from '$lib/server/db';
-import {
-	youthEvents,
-	youthNewsArticles,
-	childrensEvents,
-	childrensNewsArticles,
-	newsArticles,
-	bibleEducationEvents
-} from '$lib/server/db/schema';
+import { newsArticles, eventSchemas, Event } from '$lib/server/db/schema';
 import { env } from '$env/dynamic/private';
 import { lte, desc, asc, or, sql, eq, gte, isNull, and, lt } from 'drizzle-orm';
 import { unionAll } from 'drizzle-orm/pg-core';
-const event_limit = 5;
-const news_limit = 4;
-
-export async function load({ params }) {
+export async function load() {
 	const today = new Date().toISOString(); // Convert Date to ISO string
-	const models = [youthEvents, childrensEvents, bibleEducationEvents];
 
-	const queries = models.map((model) =>
-		db
-			.select({
-				id: model.id,
-				title: model.title,
-				startAt: model.startAt,
-				endAt: model.endAt,
-				slug: model.slug
-			})
-			.from(model)
-			.where(lte(today, model.startAt)) // This needs to be improved
-			.limit(event_limit)
+	const eventsSchema = unionAll(
+		...eventSchemas.map((eventSchema) =>
+			db
+				.select({
+					id: eventSchema.id,
+					title: eventSchema.title,
+					startAt: eventSchema.startAt,
+					endAt: eventSchema.endAt,
+					slug: eventSchema.slug
+				})
+				.from(eventSchema)
+		)
 	);
 
-	const combinedEvents = unionAll(...queries).as('combinedEvents');
-
 	const [events, news_articles] = await Promise.all([
-		db.select().from(combinedEvents).orderBy(desc(combinedEvents.startAt)).limit(event_limit),
-		db.select().from(newsArticles).orderBy(desc(newsArticles.date)).limit(news_limit)
+		eventsSchema
+			.where(lte(today, Event.startAt))
+			.orderBy(sql`start_at ASC`)
+			.limit(5),
+		db.select().from(newsArticles).orderBy(desc(newsArticles.date)).limit(5)
 	]);
-	//
-	// console.log({ events, news_articles });
-	const es = events.map((event) => {
+	events.map((event: Event) => {
 		console.log(event.startAt);
 		event.month_short = new Intl.DateTimeFormat('ru-RU', { month: 'short' })
 			.format(new Date(event.startAt))
@@ -57,10 +45,12 @@ export async function load({ params }) {
 			year: 'numeric'
 		}).format(new Date(event.startAt))} ${
 			event.endAt
-				? ' - ' +
-					new Intl.DateTimeFormat('ru-RU', { day: 'numeric', month: 'long' }).format(
-						new Date(event.endAt)
-					)
+				? ' — ' +
+					new Intl.DateTimeFormat('ru-RU', {
+						day: 'numeric',
+						month: 'long',
+						year: 'numeric'
+					}).format(new Date(event.endAt))
 				: ''
 		}`;
 
