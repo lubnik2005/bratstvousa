070d272bca38dab8c1c1aff11e78cc94231d3f65
lubnik2005 <25041454+lubnik2005@users.noreply.github.com>
2025-01-08 21:58:07 -0800

    Fixed regions for churches

--- app/src/routes/churches/+page.server.ts
+++ app/src/routes/churches/+page.server.ts
@@ -5,7 +5,18 @@ import { desc } from 'drizzle-orm';
 
 export async function load() {
 	return {
-		churches: await db.select().from(churches).orderBy(desc(churches.state)),
+		churches: (await db.select().from(churches).orderBy(desc(churches.state))).sort((a, b) => {
+			const stateA = a.city.split(', ')[1];
+			const stateB = b.city.split(', ')[1];
+
+			if (stateA < stateB) return -1;
+			if (stateA > stateB) return 1;
+
+			// If states are the same, compare cities
+			const cityA = a.city.split(', ')[0];
+			const cityB = b.city.split(', ')[0];
+			return cityA.localeCompare(cityB);
+		}),
 		media_url: env.MEDIA_URL
 	};
 }
