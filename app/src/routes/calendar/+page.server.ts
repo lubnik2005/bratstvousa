4cbede3542b36c826f5ec3dac088852dc7c4c256
lubnik2005 <25041454+lubnik2005@users.noreply.github.com>
2025-01-08 21:58:21 -0800

    Fixed calendar display

--- app/src/routes/calendar/+page.server.ts
+++ app/src/routes/calendar/+page.server.ts
@@ -1,10 +1,21 @@
 import { db } from '$lib/server/db';
-import { youthEvents } from '$lib/server/db/schema';
+import { Event, eventSchemas } from '$lib/server/db/schema';
+import { unionAll } from 'drizzle-orm/mysql-core';
 
-export async function load({ params }) {
-	const youth_events = await db.select().from(youthEvents);
+export async function load() {
+	const events: Event[] = await unionAll(
+		...eventSchemas.map((eventSchema) =>
+			db
+				.select({
+					id: eventSchema.id,
+					title: eventSchema.title,
+					startAt: eventSchema.startAt,
+					endAt: eventSchema.endAt,
+					slug: eventSchema.slug
+				})
+				.from(eventSchema)
+		)
+	);
 
-	return {
-		youth_events
-	};
+	return { events };
 }
--- app/src/routes/calendar/+page.svelte
+++ app/src/routes/calendar/+page.svelte
@@ -1,35 +1,26 @@
 <script lang="ts">
+	import timeGridPlugin from '@fullcalendar/timegrid';
+	import ruLocale from '@fullcalendar/core/locales/ru';
+
 	import { onMount } from 'svelte';
 	import { Calendar } from '@fullcalendar/core';
 	import dayGridPlugin from '@fullcalendar/daygrid';
-	let { data } = $props();
+	export let data;
 
 	let calendar: Calendar;
-
-	// Define your events
-	const events = [
-		{
-			title: 'Meeting',
-			start: '2024-12-01T10:00:00',
-			end: '2024-12-01T12:00:00',
-			url: 'https://google.com'
-		},
-		{
-			title: 'Lunch',
-			start: '2024-12-02T13:00:00'
-		},
-		{
-			title: 'Conference',
-			start: '2024-12-05',
-			end: '2024-12-07'
-		}
-	];
 
 	onMount(() => {
 		const calendarEl = document.getElementById('calendar')!;
 		calendar = new Calendar(calendarEl, {
-			plugins: [dayGridPlugin],
-			events: data.youth_events.map((e) => ({
+			plugins: [dayGridPlugin, timeGridPlugin],
+			initialView: 'dayGridMonth',
+			headerToolbar: {
+				left: 'prev,today,next',
+				center: 'title',
+				right: 'dayGridMonth,dayGridWeek,timeGridDay'
+			},
+			locales: [ruLocale],
+			events: data.events.map((e) => ({
 				title: e.title,
 				start: e.startAt,
 				end: e.endAt,
@@ -44,6 +35,8 @@
 	});
 </script>
 
+}
+
 <div class="container-xxl py-6">
 	<div class="container">
 		<div class="section-header mx-auto mb-5 text-center" style="max-width: 500px;">
@@ -54,5 +47,38 @@
 </div>
 
 <style>
-	/* Add styles for the calendar if needed */
+	:root {
+		--fc-small-font-size: 0.85em;
+		--fc-page-bg-color: #fff;
+		--fc-neutral-bg-color: rgba(208, 208, 208, 0.3);
+		--fc-neutral-text-color: #808080;
+		--fc-border-color: #ddd;
+
+		--fc-button-text-color: #fff;
+		--fc-button-bg-color: #2c2b29;
+		--fc-button-border-color: #2c2b29;
+		--fc-button-hover-bg-color: #555555;
+		--fc-button-hover-border-color: #555555;
+		--fc-button-active-bg-color: #555555;
+		--fc-button-active-border-color: #555555;
+
+		--fc-event-bg-color: #5a4a42;
+		--fc-event-border-color: #2c2b29;
+		--fc-event-text-color: #fff;
+		--fc-event-selected-overlay-color: rgba(0, 0, 0, 0.25);
+
+		--fc-more-link-bg-color: #d0d0d0;
+		--fc-more-link-text-color: inherit;
+
+		--fc-event-resizer-thickness: 8px;
+		--fc-event-resizer-dot-total-width: 8px;
+		--fc-event-resizer-dot-border-width: 1px;
+
+		--fc-non-business-color: rgba(215, 215, 215, 0.3);
+		--fc-bg-event-color: rgb(143, 223, 130);
+		--fc-bg-event-opacity: 0.3;
+		--fc-highlight-color: rgba(188, 232, 241, 0.3);
+		--fc-today-bg-color: rgba(255, 220, 40, 0.15);
+		--fc-now-indicator-color: red;
+	}
 </style>
