a64835381c9ba5b6372f08409cc15156a3f0ce0a
lubnik2005 <25041454+lubnik2005@users.noreply.github.com>
2025-01-08 21:56:02 -0800

    Added region

--- admin/database/migrations/2024_12_24_001255_create_bible_education_events_table.php
+++ admin/database/migrations/2024_12_24_001255_create_bible_education_events_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('slug')->nullable();
             $table->unsignedInteger('author_id')->nullable();
             $table->string('description')->nullable();
+            $table->string('region')->nullable();
             $table->text('content')->nullable();
             $table->string('thumbnail')->nullable();
             $table->string('featured_image')->nullable();
