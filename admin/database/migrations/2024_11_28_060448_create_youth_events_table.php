8b12cc18ca941345ed1a9637118169f9d00cbcd3
lubnik2005 <25041454+lubnik2005@users.noreply.github.com>
2025-01-08 21:55:14 -0800

    Updated table for youth events

--- admin/database/migrations/2024_11_28_060448_create_youth_events_table.php
+++ admin/database/migrations/2024_11_28_060448_create_youth_events_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('youth_events', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->string('title')->nullable();
             $table->string('slug')->nullable();
             $table->unsignedInteger('author_id')->nullable();
             $table->string('description')->nullable();
