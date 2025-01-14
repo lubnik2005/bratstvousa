a03d9a53d6603c17882827e0db80be68b37790c4
lubnik2005 <25041454+lubnik2005@users.noreply.github.com>
2025-01-14 15:23:10 -0800

    Added typing to events

--- admin/app/Models/YouthEvent.php
+++ admin/app/Models/YouthEvent.php
@@ -7,26 +7,22 @@ use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
-
 class YouthEvent extends Model implements HasMedia
 {
     use InteractsWithMedia;
 
+    public function registerMediaConversions(?Media $media = null): void
+    {
+        $this->addMediaConversion('gallery_thumbnails')
+            ->width(130)
+            ->height(130);
+    }
 
-
-public function registerMediaConversions(Media $media = null): void
-{
-    $this->addMediaConversion('thumb')
-        ->width(130)
-        ->height(130);
-}
-
-public function registerMediaCollections(): void
-{
-    $this->addMediaCollection('main')->singleFile();
-    $this->addMediaCollection('my_multi_collection');
-}
-
+    public function registerMediaCollections(): void
+    {
+        // $this->addMediaCollection('main')->singleFile();
+        $this->addMediaCollection('gallery');
+    }
 
     protected $casts = ['start_at' => 'date', 'end_at' => 'date'];
 }
