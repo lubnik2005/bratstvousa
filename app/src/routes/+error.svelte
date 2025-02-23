0dd5055fc0f53d4259f3054c843c856b162bb916
lubnik2005 <25041454+lubnik2005@users.noreply.github.com>
2025-02-22 21:47:40 -0800

    Updated error message

--- app/src/routes/+error.svelte
+++ app/src/routes/+error.svelte
@@ -5,19 +5,15 @@
 
 Error page
 
-
 <div class="container-fluid bg-icon mb-5">
 	<div class="container">
-		<div class="row ">
-<Header title={page.status.toString()} subtitle={page.error?.message} />
-    </div>
+		<div class="row">
+			<Header title={page.status.toString()} subtitle={page.error?.message} />
+		</div>
 		<div class="row align-items-center">
-<a
-        style="max-width: 20rem;"
-  class="btn btn-md btn-secondary rounded-pill px-5 py-3"
-  href="/">Вернуться домой</a
->
+			<a style="max-width: 20rem;" class="btn btn-md btn-secondary rounded-pill px-5 py-3" href="/"
+				>Вернуться домой</a
+			>
+		</div>
+	</div>
 </div>
-</div>
-</div>
-
