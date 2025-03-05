e85fe67b91ce170226d61f8ac3adf38b91097473
lubnik2005 <25041454+lubnik2005@users.noreply.github.com>
2025-03-04 18:46:52 -0800

    Moved shared fields for articles into shared class

--- admin/app/Nova/Traits/SharedFields.php
+++ admin/app/Nova/Traits/SharedFields.php
@@ -41,4 +41,37 @@ class SharedFields
             Files::make('Featured Document', 'featured_document')->help('For certain types of resoures, this will appear as the main file to download to get more information about an event.'),
         ];
     }
+
+    public static function articleFields(){
+        $disk = config('filesystems.default');
+        return [
+            ID::make()->sortable(),
+            Text::make('Title')->rules('required')->required(),
+            Slug::make('Slug')->from('title'),
+            Text::make('Description'),
+            Date::make('Date'),
+            Image::make('Featured Image', 'featured_image', $disk)->path('upfiles/news'),
+            Image::make('Thumbnail', 'thumbnail', $disk)->path('/upfiles/page'),
+            Trix::make('Content')->withFiles($disk)->path('/upfiles/page')
+                ->fillUsing(function ($request, $model, $attribute, $requestAttribute) {
+                    $content = $request->input($requestAttribute);
+                    // Replace any width attribute with width="100%"
+                    $content = preg_replace('/width="[^"]*"/', 'width="100%"', $content);
+                    $content = preg_replace('/height="[^"]*"/', '', $content);
+                    // Set the modified value
+                    $model->{$attribute} = $content;
+                }),
+            Images::make('Gallery', 'gallery') // Media collection name: 'gallery'
+                ->nullable()
+                ->showDimensions() // Display dimensions directly
+                ->conversionOnDetailView('gallery_thumbnails') // Conversion for detail view
+                ->conversionOnIndexView('gallery_thumbnails') // Conversion for index view
+                ->conversionOnForm('gallery_thumbnails') // Conversion for form view
+                ->fullSize(), // Allow full-size viewing in a column
+            Files::make('Playlist', 'playlist'),
+            Files::make('Documents', 'documents'),
+            File::make('Featured Document', 'featured_document', $disk)->path('/upfiles/page')->help('For certain types of resoures, this will appear as the main file to download to get more information about an event.'),
+
+        ]
+    }
 }
