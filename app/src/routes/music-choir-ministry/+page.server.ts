e892068beef5b7999aa5fee980a24d06dfd45b50
lubnik2005 <25041454+lubnik2005@users.noreply.github.com>
2025-03-20 20:38:51 -0700

    Updated music choir ministry

--- app/src/routes/music-choir-ministry/+page.server.ts
+++ app/src/routes/music-choir-ministry/+page.server.ts
@@ -1,10 +1,19 @@
 import { env } from '$env/dynamic/private';
 import { db } from '$lib/server/db';
-import { childrensEvents, childrensFiles } from '$lib/server/db/schema';
+import { getMinistryEvents, getMinistryNewsArticles } from '$lib/server/db/queries';
+import {
+	childrensEvents,
+	childrensFiles,
+	familyEvents,
+	familyNewsArticles,
+	musicEvents
+} from '$lib/server/db/schema';
 import { isNull, and, desc, or, gte, lt } from 'drizzle-orm';
 
 export async function load({ params }) {
 	return {
-		media_url: env.MEDIA_URL
+		media_url: env.MEDIA_URL,
+		events: await getMinistryEvents(musicEvents),
+		articles: await getMinistryNewsArticles(familyNewsArticles)
 	};
 }
