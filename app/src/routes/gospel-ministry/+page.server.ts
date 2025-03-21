e100799791001e278811d3b85c5173c054cdd42d
lubnik2005 <25041454+lubnik2005@users.noreply.github.com>
2025-03-21 14:59:38 -0700

    Removed wrong events for gospel articles

--- app/src/routes/gospel-ministry/+page.server.ts
+++ app/src/routes/gospel-ministry/+page.server.ts
@@ -1,15 +1,12 @@
-import { db } from '$lib/server/db';
-import { bibleEducationEvents, bibleEducationNewsArticles } from '$lib/server/db/schema';
+import { gospelEvents } from '$lib/server/db/schema';
 import { env } from '$env/dynamic/private';
-import { formatDate } from '$lib/helpers';
 
-import { eq, lt, gte, or, and, isNull } from 'drizzle-orm/expressions';
 import { getMinistryEvents, getMinistryNewsArticles } from '$lib/server/db/queries';
 
-export async function load({ params }) {
+export async function load() {
 	return {
-		events: await getMinistryEvents(bibleEducationEvents),
-		articles: await getMinistryNewsArticles(bibleEducationNewsArticles),
+		events: await getMinistryEvents(gospelEvents),
+		articles: [],
 		media_url: env.MEDIA_URL
 	};
 }
