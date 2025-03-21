6d853e97844500eccedd98a9dbdd16297ddfa94b
lubnik2005 <25041454+lubnik2005@users.noreply.github.com>
2025-03-21 10:53:54 -0700

    Made FamilyNewsArticles visible on the admin site

--- admin/app/Policies/FamilyNewsArticlePolicy.php
+++ admin/app/Policies/FamilyNewsArticlePolicy.php
@@ -4,63 +4,62 @@ namespace App\Policies;
 
 use App\Models\FamilyNewsArticle;
 use App\Models\User;
-use Illuminate\Auth\Access\Response;
 
 class FamilyNewsArticlePolicy
 {
     /**
-     * Determine whether the user can view any models.
+     * Determine whether the user can view any family news articles.
      */
     public function viewAny(User $user): bool
     {
-        return false;
+        return $user->hasPermissionTo('view family_news_articles');
     }
 
     /**
-     * Determine whether the user can view the model.
+     * Determine whether the user can view a specific family news article.
      */
     public function view(User $user, FamilyNewsArticle $familyNewsArticle): bool
     {
-        return false;
+        return $user->hasPermissionTo('view family_news_articles');
     }
 
     /**
-     * Determine whether the user can create models.
+     * Determine whether the user can create family news articles.
      */
     public function create(User $user): bool
     {
-        return false;
+        return $user->hasPermissionTo('create family_news_articles');
     }
 
     /**
-     * Determine whether the user can update the model.
+     * Determine whether the user can update the family news article.
      */
     public function update(User $user, FamilyNewsArticle $familyNewsArticle): bool
     {
-        return false;
+        return $user->hasPermissionTo('edit family_news_articles');
     }
 
     /**
-     * Determine whether the user can delete the model.
+     * Determine whether the user can delete the family news article.
      */
     public function delete(User $user, FamilyNewsArticle $familyNewsArticle): bool
     {
-        return false;
+        return $user->hasPermissionTo('delete family_news_articles');
     }
 
     /**
-     * Determine whether the user can restore the model.
+     * Determine whether the user can restore a deleted family news article.
      */
     public function restore(User $user, FamilyNewsArticle $familyNewsArticle): bool
     {
-        return false;
+        return $user->hasPermissionTo('delete family_news_articles');
     }
 
     /**
-     * Determine whether the user can permanently delete the model.
+     * Determine whether the user can permanently delete the family news article.
      */
     public function forceDelete(User $user, FamilyNewsArticle $familyNewsArticle): bool
     {
-        return false;
+        return $user->hasPermissionTo('delete family_news_articles');
     }
 }
