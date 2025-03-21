76b14ef26d0da37bb74a704e637d98b679197288
lubnik2005 <25041454+lubnik2005@users.noreply.github.com>
2025-03-21 14:58:47 -0700

    Fixed typo in articles in music ministry

--- app/src/routes/music-choir-ministry/+page.server.ts
+++ app/src/routes/music-choir-ministry/+page.server.ts
@@ -1,19 +1,11 @@
 import { env } from '$env/dynamic/private';
-import { db } from '$lib/server/db';
 import { getMinistryEvents, getMinistryNewsArticles } from '$lib/server/db/queries';
-import {
-	childrensEvents,
-	childrensFiles,
-	familyEvents,
-	familyNewsArticles,
-	musicEvents
-} from '$lib/server/db/schema';
-import { isNull, and, desc, or, gte, lt } from 'drizzle-orm';
+import { musicEvents, musicNewsArticles } from '$lib/server/db/schema';
 
-export async function load({ params }) {
+export async function load() {
 	return {
 		media_url: env.MEDIA_URL,
 		events: await getMinistryEvents(musicEvents),
-		articles: await getMinistryNewsArticles(familyNewsArticles)
+		articles: await getMinistryNewsArticles(musicNewsArticles)
 	};
 }
