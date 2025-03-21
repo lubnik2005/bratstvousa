9fce1ce408e1e27d1f44fcc709e205eef282f023
lubnik2005 <25041454+lubnik2005@users.noreply.github.com>
2025-03-21 10:40:32 -0700

    Added generic page component to family ministry page

--- app/src/routes/family-ministry/+page.server.ts
+++ app/src/routes/family-ministry/+page.server.ts
@@ -1,18 +1,11 @@
+import { familyNewsArticles, familyEvents } from '$lib/server/db/schema';
 import { env } from '$env/dynamic/private';
-import { db } from '$lib/server/db';
 import { getMinistryEvents, getMinistryNewsArticles } from '$lib/server/db/queries';
-import {
-	childrensEvents,
-	childrensFiles,
-	familyEvents,
-	familyNewsArticles
-} from '$lib/server/db/schema';
-import { isNull, and, desc, or, gte, lt } from 'drizzle-orm';
 
-export async function load({ params }) {
+export async function load() {
 	return {
-		media_url: env.MEDIA_URL,
 		events: await getMinistryEvents(familyEvents),
-		articles: await getMinistryNewsArticles(familyNewsArticles)
+		news_articles: await getMinistryNewsArticles(familyNewsArticles),
+		media_url: env.MEDIA_URL
 	};
 }
