8bb8551904c2e3910e1ba69556b151013bd16b97
lubnik2005 <25041454+lubnik2005@users.noreply.github.com>
2025-03-20 20:39:02 -0700

    Updated all news pages

--- app/src/routes/news/+page.server.ts
+++ app/src/routes/news/+page.server.ts
@@ -1,8 +1,10 @@
 import { db } from '$lib/server/db';
-import { newsArticles } from '$lib/server/db/schema';
-import { desc, eq, isNotNull, count } from 'drizzle-orm';
+import { bibleEducationNewsArticles, eventSchemas, newsArticles } from '$lib/server/db/schema';
+import { desc, eq, isNotNull, count, sql } from 'drizzle-orm';
 import { env } from '$env/dynamic/private';
 import { formatDate } from '$lib/helpers';
+import { unionAll } from 'drizzle-orm/mysql-core';
+import { newsArticlesSchema, newsArticlesSchemaOrdered } from '$lib/server/db/queries';
 
 export async function load({ params, url }) {
 	// Get the page parameter from the query string, default to page 1
@@ -10,25 +12,25 @@ export async function load({ params, url }) {
 	const perPage = 30;
 	const offset = (page - 1) * perPage;
 
-	// Query the database for the current page
-	const news_articles = (
-		await db
-			.select()
-			.from(newsArticles)
-			.where(isNotNull(newsArticles.date))
-			.orderBy(desc(newsArticles.date))
-			.limit(perPage)
-			.offset(offset)
-	).map((a) => ({ date_string: formatDate(a.date), ...a }));
-
 	// Query the total count for pagination controls
-	const totalArticles = (await db.select({ count: count() }).from(newsArticles))[0].count;
+	const b = await db.select().from(newsArticlesSchemaOrdered);
+	const a = await db.select({ count: count() }).from(newsArticlesSchemaOrdered);
+	const news_articles_count = (
+		await db.select({ count: count() }).from(newsArticlesSchemaOrdered)
+	)[0].count;
+	// Query the database for the current page
+	const news_articles = (await newsArticlesSchemaOrdered.limit(perPage).offset(offset)).map(
+		(a) => ({
+			date_string: formatDate(a.date),
+			...a
+		})
+	);
 
 	return {
 		news_articles,
 		page,
 		perPage,
-		totalArticles,
+		news_articles_count,
 		media_url: env.MEDIA_URL
 	};
 }
