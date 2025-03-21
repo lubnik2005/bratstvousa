01ed7ba34ca55569256d2f5d226ea6c5afe37a35
lubnik2005 <25041454+lubnik2005@users.noreply.github.com>
2025-03-20 20:38:37 -0700

    Updated family ministry

--- app/src/routes/family-ministry/+page.server.ts
+++ app/src/routes/family-ministry/+page.server.ts
@@ -1,10 +1,18 @@
 import { env } from '$env/dynamic/private';
 import { db } from '$lib/server/db';
-import { childrensEvents, childrensFiles } from '$lib/server/db/schema';
+import { getMinistryEvents, getMinistryNewsArticles } from '$lib/server/db/queries';
+import {
+	childrensEvents,
+	childrensFiles,
+	familyEvents,
+	familyNewsArticles
+} from '$lib/server/db/schema';
 import { isNull, and, desc, or, gte, lt } from 'drizzle-orm';
 
 export async function load({ params }) {
 	return {
-		media_url: env.MEDIA_URL
+		media_url: env.MEDIA_URL,
+		events: await getMinistryEvents(familyEvents),
+		articles: await getMinistryNewsArticles(familyNewsArticles)
 	};
 }
