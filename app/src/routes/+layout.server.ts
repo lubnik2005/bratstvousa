c58a19445f18694fec4baf110bf3fc88bb98a942
lubnik2005 <25041454+lubnik2005@users.noreply.github.com>
2025-03-26 12:00:21 -0700

    Implemented first couple of settings into sveltekit templates

--- app/src/routes/+layout.server.ts
+++ app/src/routes/+layout.server.ts
@@ -1,7 +1,10 @@
 import { env } from '$env/dynamic/private';
+import { setting } from '$lib/server/db/queries';
 
 export async function load() {
 	return {
-		media_url: env.MEDIA_URL
+		media_url: env.MEDIA_URL,
+		donation_url: await setting('donation_url'),
+		donation_tokenuid: await setting('donation_tokenuid')
 	};
 }
--- app/src/routes/short-introduction/+page.server.ts
+++ app/src/routes/short-introduction/+page.server.ts
@@ -1,7 +1,9 @@
 import { env } from '$env/dynamic/private';
+import { setting } from '$lib/server/db/queries';
 
 export async function load({ params }) {
 	return {
-		media_url: env.MEDIA_URL
+		media_url: env.MEDIA_URL,
+		short_introduction_content: await setting('short_introduction_content')
 	};
 }
