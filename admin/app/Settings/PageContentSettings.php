8ec88781196f7c4961fbf9c50adb239265427d5a
lubnik2005 <25041454+lubnik2005@users.noreply.github.com>
2025-03-25 18:37:25 -0700

    Created page content settings

---
+++ admin/app/Settings/PageContentSettings.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Settings;
+
+use Spatie\LaravelSettings\Settings;
+
+class PageContentSettings extends Settings
+{
+
+    public string $short_introduction_content = '';
+
+    public static function group(): string
+    {
+        return 'Pages';
+    }
+}
