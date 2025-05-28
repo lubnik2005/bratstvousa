48acee1ad9f4863b41c55495869f9b08584dc999
lubnik2005 <25041454+lubnik2005@users.noreply.github.com>
2025-05-27 21:49:06 -0700

    No, it did not work

--- admin/config/filesystems.php
+++ admin/config/filesystems.php
@@ -32,7 +32,7 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('upfiles'),
+            'root' => storage_path('app/upfiles'),
             'url' => env('MEDIA_URL'),
             'serve' => true,
             'throw' => false,
