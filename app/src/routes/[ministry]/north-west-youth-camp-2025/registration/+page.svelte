19b8fc03b35d17781470c11dd568abbf9fcd3ec3
lubnik2005 <25041454+lubnik2005@users.noreply.github.com>
2025-09-29 08:16:17 -0700

    Changed zelle email

--- app/src/routes/[ministry]/north-west-youth-camp-2025/registration/+page.svelte
+++ app/src/routes/[ministry]/north-west-youth-camp-2025/registration/+page.svelte
@@ -14,7 +14,7 @@
   const CAMP_ADDRESS = "1 Muddy Rd, Antelope, OR 97001";
 
   const CASHAPP = "$TIM0XA";
-  const ZELLE = "youth@bratstvousa.com";
+  const ZELLE = "gyouth5116@bratstvousa.com";
 
   // External consent form as embed (Cognito)
   const CONSENT_URL =
