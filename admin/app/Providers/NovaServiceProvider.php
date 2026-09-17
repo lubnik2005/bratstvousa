abd614edbf1ed8f880bc4f4da8a44e572997fd88
nik-lubyanoy <nik.lubyanoy@vastspace.com>
2026-09-17 08:27:53 -0700

    Nova: group sidebar into Bratstvo USA / North-West Camps / Camp Paradise / Administration

--- admin/app/Providers/NovaServiceProvider.php
+++ admin/app/Providers/NovaServiceProvider.php
@@ -4,7 +4,11 @@ namespace App\Providers;
 
 use Dniccum\NovaDocumentation\NovaDocumentation;
 use Ferdiunal\NovaSettings\NovaSettings;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
+use Laravel\Nova\Menu\MenuGroup;
+use Laravel\Nova\Menu\MenuItem;
+use Laravel\Nova\Menu\MenuSection;
 use Laravel\Nova\Nova;
 use Laravel\Nova\NovaApplicationServiceProvider;
 
@@ -30,6 +34,76 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
             \App\Nova\Paradise\Form::class,
             \App\Nova\Paradise\FormAnswer::class,
         ]);
+
+        Nova::mainMenu(fn (Request $request) => $this->mainMenu($request));
+    }
+
+    /**
+     * Build the sidebar. Nova's default lumps every resource under a single
+     * "Resources" heading; we group them by the site/product they belong to.
+     * Each item still honours its resource policy via MenuSection::resource().
+     *
+     * @return array<int, \Laravel\Nova\Menu\MenuSection>
+     */
+    protected function mainMenu(Request $request): array
+    {
+        return array_values(array_filter([
+            MenuSection::dashboard(\App\Nova\Dashboards\Main::class)->icon('chart-bar'),
+
+            MenuSection::make('Bratstvo USA', [
+                MenuGroup::make('Events', [
+                    MenuItem::resource(\App\Nova\GeneralEvent::class),
+                    MenuItem::resource(\App\Nova\YouthEvent::class),
+                    MenuItem::resource(\App\Nova\ChildrensEvent::class),
+                    MenuItem::resource(\App\Nova\FamilyEvent::class),
+                    MenuItem::resource(\App\Nova\GospelEvent::class),
+                    MenuItem::resource(\App\Nova\MusicEvent::class),
+                    MenuItem::resource(\App\Nova\BibleEducationEvent::class),
+                ]),
+                MenuGroup::make('News', [
+                    MenuItem::resource(\App\Nova\NewsArticle::class),
+                    MenuItem::resource(\App\Nova\YouthNewsArticle::class),
+                    MenuItem::resource(\App\Nova\ChildrensNewsArticle::class),
+                    MenuItem::resource(\App\Nova\FamilyNewsArticle::class),
+                    MenuItem::resource(\App\Nova\MusicNewsArticle::class),
+                    MenuItem::resource(\App\Nova\BibleEducationNewsArticle::class),
+                ]),
+                MenuGroup::make('Content', [
+                    MenuItem::resource(\App\Nova\Church::class),
+                    MenuItem::resource(\App\Nova\ChildrensFile::class),
+                    MenuItem::resource(\App\Nova\ChildrensFileCategory::class),
+                    MenuItem::resource(\App\Nova\FormSubmission::class),
+                ]),
+            ])->icon('globe-alt')->collapsable(),
+
+            MenuSection::make('North-West Camps', [
+                MenuItem::resource(\App\Nova\CampRegistration::class),
+                MenuItem::lens(\App\Nova\CampRegistration::class, \App\Nova\Lenses\CashReconciliation::class)
+                    ->canSee(fn ($request) => \App\Nova\CampRegistration::authorizedToViewAny($request)),
+                MenuItem::resource(\App\Nova\CashEligibilityRule::class),
+                MenuItem::resource(\App\Nova\ZeffyPayment::class),
+                MenuItem::resource(\App\Nova\YouthLeader::class),
+                MenuItem::resource(\App\Nova\EmailLog::class),
+            ])->icon('ticket')->collapsable(),
+
+            MenuSection::make('Camp Paradise', [
+                MenuItem::resource(\App\Nova\Paradise\Event::class),
+                MenuItem::resource(\App\Nova\Paradise\Reservation::class),
+                MenuItem::resource(\App\Nova\Paradise\Room::class),
+                MenuItem::resource(\App\Nova\Paradise\Cot::class),
+                MenuItem::resource(\App\Nova\Paradise\Form::class),
+                MenuItem::resource(\App\Nova\Paradise\FormAnswer::class),
+            ])->icon('home')->collapsable(),
+
+            MenuSection::make('Administration', [
+                MenuItem::resource(\App\Nova\User::class),
+                MenuItem::resource(\Pktharindu\NovaPermissions\Nova\Role::class),
+                MenuItem::resource(\App\Nova\AuditLog::class),
+            ])->icon('shield-check')->collapsable(),
+
+            (new NovaSettings)->menu($request),
+            (new NovaDocumentation)->menu($request),
+        ]));
     }
 
     /**
